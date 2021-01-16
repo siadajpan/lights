1604e8ab3f8343325bdb52b3488e609e585f1494
Karol <siadajpan@o2.pl>
2021-01-16 18:31:00 +0100

    Fix logging level

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ import logging
 from lights.mqtt_client.mqtt_client import MQTTClient
 
 if __name__ == '__main__':
-    logging.getLogger().setLevel(logging.DEBUG)
+    logging.root.setLevel(logging.DEBUG)
+    logging.info('Starting lights')
     client = MQTTClient()
     client.connect()
     client.loop_forever()
