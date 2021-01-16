403aac8e095b5263cd936e22bbcff68f930db526
Karol <siadajpan@o2.pl>
2021-01-16 18:20:31 +0100

    Set level of logging to debug

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import logging
+
 from lights.mqtt_client.mqtt_client import MQTTClient
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.DEBUG)
     client = MQTTClient()
     client.connect()
     client.loop_forever()
