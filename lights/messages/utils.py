70c719fb3780f400df51f57494968899d5fbfb16
Karol <siadajpan@o2.pl>
2021-01-17 10:24:46 +0100

    Separate message evaluation from color evalution

--- lights/messages/utils.py
+++ lights/messages/utils.py
@@ -3,20 +3,27 @@ from typing import Tuple
 from lights.errors.incorrect_payload import IncorrectPayloadException
 
 
-def check_color_message(message) -> Tuple[int, int, int]:
-    error_message = f'Color payload formatted incorrectly, ' \
-                    f'expected Tuple[uint8, uint8, uint8], ' \
-                    f'got {message}'
+def evaluate_message(message) -> bool:
     try:
         message = eval(message)
+        return message
+    except SyntaxError:
+        error_message = f'Message payload formatted incorrectly, ' \
+                        f'got {message}'
+        raise IncorrectPayloadException(error_message)
+
+
+def check_color_message(message) -> Tuple[int, int, int]:
+    try:
+        message = evaluate_message(message)
         assert isinstance(message, tuple)
         for el in message:
             assert isinstance(el, int)
             assert 0 <= el <= 255
 
         return message
-
     except AssertionError:
-        raise IncorrectPayloadException(error_message)
-    except SyntaxError:
+        error_message = f'Color payload formatted incorrectly, ' \
+                        f'expected Tuple[uint8, uint8, uint8], ' \
+                        f'got {message}'
         raise IncorrectPayloadException(error_message)
