145da2d214fb7f261023cd6f5a7c4f3d214ec91c
Karol <siadajpan@o2.pl>
2021-01-17 11:26:29 +0100

    Fix returning bool on executing message

--- lights/messages/message_manager.py
+++ lights/messages/message_manager.py
@@ -4,10 +4,11 @@ from typing import List
 from lights.errors.incorrect_topic_exception import IncorrectTopicException
 from lights.messages.abstract_message import AbstractMessage
 from lights.messages.turn_off import TurnOff
+from lights.messages.turn_slowly_static import TurnSlowlyStatic
 from lights.messages.turn_static_color import TurnStaticColor
 from lights.settings import settings
 
-MESSAGES = [TurnOff(), TurnStaticColor()]
+MESSAGES = [TurnOff(), TurnStaticColor(), TurnSlowlyStatic()]
 
 
 class MessageManager:
@@ -15,7 +16,7 @@ class MessageManager:
         self.messages: List[AbstractMessage] = MESSAGES
         self.logger = logging.getLogger(self.__class__.__name__)
 
-    def execute_message(self, payload: str, topic=settings.Mqtt.TOPIC) -> bool:
+    def execute_message(self, payload: str, topic=settings.Mqtt.TOPIC):
         self.logger.debug(f'Searching for message topic: {topic}, payload: {payload}')
         for message in self.messages:
             if message.topic != topic:
