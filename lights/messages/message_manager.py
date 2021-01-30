4bd2f8669e328a0e7199f02d0593c720370f0e2a
Karol Misiarz <karol.misiarz@robovision.eu>
2021-01-30 15:37:39 +0100

    Pass execution on state messages

--- lights/messages/message_manager.py
+++ lights/messages/message_manager.py
@@ -1,9 +1,10 @@
 import logging
 from queue import Queue
 from threading import Thread
-from typing import List, Callable
+from typing import List, Callable, Optional
 
-from lights.errors.incorrect_topic_exception import IncorrectTopicException
+import paho.mqtt.client as mqtt
+
 from lights.errors.lights_exception import LightsException
 from lights.messages.abstract_message import AbstractMessage
 from lights.messages.empty import Empty
@@ -11,7 +12,6 @@ from lights.messages.turn_off import TurnOff
 from lights.messages.turn_slowly_static import TurnSlowlyStatic
 from lights.messages.turn_static import TurnStatic
 from lights.settings import settings
-import paho.mqtt.client as mqtt
 
 MESSAGES = [TurnOff(), TurnStatic(), TurnSlowlyStatic(), Empty()]
 
@@ -33,14 +33,16 @@ class MessageManager(Thread):
 
     def execute_message(self, topic: str, payload: str):
         message = self.check_message(topic)
+        if not message:
+            return
         try:
             message.execute(payload)
         except Exception as ex:
             self._logger.error(f'Error raised during execution of message. '
-                              f'Exception: {ex}')
+                               f'Exception: {ex}')
             raise ex
 
-    def check_message(self, topic) -> AbstractMessage:
+    def check_message(self, topic) -> Optional[AbstractMessage]:
         self._logger.debug(f'Searching for message topic: {topic}')
         if topic in self._topic_registered:
             return self._messages[self._topic_registered.index(topic)]
@@ -49,15 +51,15 @@ class MessageManager(Thread):
             f'Received message not registered. Registered topics: ' \
             f'{[message.topic for message in self._messages]} got: {topic}'
 
-        self._logger.error(error_message)
+        self._logger.warning(error_message)
 
-        raise IncorrectTopicException(error_message)
+        return None
 
     def run(self) -> None:
         while not self._stop_thread:
             message: mqtt.MQTTMessage = self._message_queue.get()
             self._logger.debug(f'Got message topic: {message.topic} '
-                              f'payload: {message.payload}')
+                               f'payload: {message.payload}')
             try:
                 self.execute_message(message.topic, message.payload)
             except LightsException as ex:
